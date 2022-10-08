e5f03630f135fd700d5987c40a2302529beb1665
Curtis Jensen <curtis.jensen@gmail.com>
2022-10-08 14:19:13 -0700

    toggle showing size, health, age

--- untitled/life_display.py
+++ untitled/life_display.py
@@ -27,31 +27,42 @@ class LifeDisplay:
         self.min_size = 1
         self.max_health = 100
         self.min_health = 75
+
+        self.show_age = True
+        self.show_health = True
+        self.show_size = True
     #end def
 
     def entity_color(self, entity):
-        entity_color = (0, 0, 0)
+        r = g = b = 0
+
         if entity.health > 0:
-            if entity.age > self.max_age:
-                self.max_age = entity.age
-            if entity.size > self.max_size:
-                self.max_size = entity.size
-            if entity.size < self.min_size:
-                self.min_size = entity.size
-            if entity.health < self.min_health:
-                self.min_health = entity.health
+            if self.show_age:
+                if entity.age > self.max_age:
+                    self.max_age = entity.age
+                r = int(255.0 * entity.age / self.max_age)
+            # end if
 
-            r = int(255.0 * entity.age / self.max_age)
-            g = int(255.0 * (entity.health - self.min_health) / (self.max_health - self.min_health))
-            b = int(255.0 * (entity.size - self.min_size) / (self.max_size - self.min_size))
+            if self.show_health:
+                if entity.health < self.min_health:
+                    self.min_health = entity.health
+                g = int(255.0 * (entity.health - self.min_health) / (self.max_health - self.min_health))
+            # end if
 
-            entity_color = (r, g, b)
+            if self.show_size:
+                if entity.size > self.max_size:
+                    self.max_size = entity.size
+                if entity.size < self.min_size:
+                    self.min_size = entity.size
+
+                b = int(255.0 * (entity.size - self.min_size) / (self.max_size - self.min_size))
+            # end if
 
             if r < 0 or r > 255 or g < 0 or g > 255 or b < 0 or b > 255:
-                print('%s: %s' % (str(entity_color), str(entity)))
+                print('%s: %s' % (str((r, g, b)), str(entity)))
         # end if
 
-        return entity_color
+        return (r, g, b)
     # end def
 
     def render_rows(self, x_range, y_range):
@@ -138,6 +149,12 @@ class LifeDisplay:
                     if event.key == pygame.K_ESCAPE:
                         game_running = False
                         break
+                    elif event.key == pygame.K_a:
+                        self.show_age = not self.show_age
+                    elif event.key == pygame.K_h:
+                        self.show_health = not self.show_health
+                    elif event.key == pygame.K_s:
+                        self.show_size = not self.show_size
                 # end if
             # end for
 
