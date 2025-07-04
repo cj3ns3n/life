ccdbe2a120d31a2e5b3e7add5d58e94a5178d5ea
Curtis Jensen <curtis.jensen@gmail.com>
2025-07-04 13:14:17 -0700

    remove extra spacing

--- terminal_display.py
+++ terminal_display.py
@@ -43,25 +43,17 @@ class TerminalDisplay:
         m_deathrate = 100.0 * float(m_deaths) / float(births) if births > 0 else 0.0
 
         self.scr.addstr(0, 0, "Cycles: %s; qsize %d" % (self.comma_number(stats['cycles']), stats['qsize']))
-
         pop_text_str = 'Population %s; Males %s; Females %s                  ' % ((births - deaths), self.comma_number(stats['males']), self.comma_number(stats['females']))
         self.scr.addstr(1, 0, pop_text_str)
-
         births_text_str = 'Births: %s; (birth rate: %0.1f)    ' % (self.comma_number(births), stats['birth_rate']*100)
         self.scr.addstr(2, 0, births_text_str)
-
         deaths_text_str = 'Deaths: %s; (death rate: %0.1f) Maternal Deaths: %s (rate: %0.1f%%); Starvation Deaths: %s; Natural Deaths: %s          ' % \
                           (self.comma_number(deaths), stats['death_rate']*100, self.comma_number(m_deaths), m_deathrate, self.comma_number(s_deaths), self.comma_number(n_deaths))
         self.scr.addstr(3, 0, deaths_text_str)
-
         self.scr.addstr(4, 0, 'Avg Age: %.1f, Stdv Age: %.1f' % (stats['age_avg'], stats['age_stdev']))
-
         self.scr.addstr(5, 0, 'Avg Health: %.1f, Stdv Health: %.1f' % (stats['health_avg'], stats['health_stdev']))
-
         self.scr.addstr(6, 0, 'Avg Size: %.1f, Stdv Size: %.1f' % (stats['size_avg'], stats['size_stdev']))
-
         self.scr.addstr(7, 0, 'Nutrient Sources: %s, Avg Nutrient Level: %.1f, Stdv Nutrient Level: %.1f' % (self.comma_number(stats['nutrient_sources']), stats['nutrient_level_avg'], stats['nutrient_level_stdev']))
-
         self.scr.addstr(8, 0, 'Refreshes %s' % self.comma_number(stats['display_iterations']))
 
         self.terminal_lock.acquire()
